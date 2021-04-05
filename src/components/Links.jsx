a61dcda2c39beebc1eff2f038da9a5e5702241f1
jessegumtow <gumtow@gmail.com>
2021-04-04 21:58:50 -0500

    forms practice

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlexCenter, SectionMargin } from "../styles/layout";
-import Button from "react-bootstrap/Button";
+import { Button, Form } from "react-bootstrap";
 
 const Links = (props) => {
+  const [test, setTest] = useState("");
+  const [checkbox, setCheckbox] = useState(true);
+
+  const handleSubmit = (e) => {
+    console.log(test);
+    e.preventDefault();
+  };
+
   return (
     <div className="container-fluid" id="links">
       <div className="container">
@@ -12,6 +20,33 @@ const Links = (props) => {
               Links
             </Button>
           </FlexCenter>
+          <Form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            <Form.Group controlId="testFrom">
+              <Form.Label>Test Input</Form.Label>
+              <Form.Control
+                name="test"
+                type="text"
+                placeholder="This is a test input."
+                onChange={(e) => setTest(e.target.value)}
+                value={test}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Test Checkbox</Form.Label>
+              <Form.Control
+                name="checkbox"
+                type="checkbox"
+                checked={checkbox}
+                onChange={(e) => setCheckbox(!checkbox)}
+                value={checkbox}
+              />
+            </Form.Group>
+            <input className="submit" type="submit" value="log test" />
+          </Form>
         </SectionMargin>
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,17 @@
 import React from "react";
-// import axios from "axios";
-import About from './About';
-import Projects from './Projects';
-import Links from './Links';
+
+import About from "./About";
+import Projects from "./Projects";
+import Links from "./Links";
 import Footer from "./Footer";
 
 const Main = (props) => {
-  // const [wpData, setWpdata] = React.useState([]);
-
-  // React.useEffect(()=>{
-  //     console.log('Heelo World')
-  //     axios.get('http://134.209.114.110/wp-json/wp/v2/mySlug')
-  //       .then(res => setWpdata(res.data))
-  //       .catch(err => console.log(err))
-  // }, [])
-  // console.log('wpData=', wpData)
-
   return (
-      
     <div>
-      {/* Hello {props.name}
-      {wpData.map((slug, i) => (
-        <div key={i}>{slug.content.rendered.slice(1,-1)}</div>
-      ))} */}
-    <About />
-    <Projects />
-    <Links />
-    <Footer />
-
+      <About />
+      <Projects />
+      <Links />
+      <Footer />
     </div>
   );
 };
