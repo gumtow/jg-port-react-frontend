468d73d609705610e6270b931e9f3ca8fd72b51d
jessegumtow <gumtow@gmail.com>
2021-04-27 23:03:12 -0500

    addign some stylign to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SectionMargin } from "../styles/layout";
+import { Row, Container, Col } from "react-bootstrap";
 // import { FlexCenter } from "../styles/layout";
 
 const About = (props) => {
@@ -7,13 +8,24 @@ const About = (props) => {
     <div className="container-fluid" id="about">
       <div className="container" >
         <SectionMargin>
-          <h1>Hello,</h1>
-          <p>
-            I’m Jesse, a human-centered, front end software developer creating
-            elegant solutions to complex problems. My creative personality
-            pushes me to find opportunities to learn and develop tools that
-            improve the lives of others.
-          </p>
+          <Container>
+            <Row>
+              <Col>
+                <img id="profile-img" src="https://api.jessegumtow.com/wp-content/uploads/2021/04/Circle_jg-profile.png" alt="Jesse Gumtow" />
+              </Col>
+              <Col className="align-items-center">
+                <div className="flex-div">
+                  <h1>Hello,</h1>
+                  <p>
+                    I’m Jesse, a human-centered, front end software developer creating
+                    elegant solutions to complex problems. My creative personality
+                    pushes me to find opportunities to learn and develop tools that
+                    improve the lives of others.
+                </p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </SectionMargin>
       </div>
     </div>
