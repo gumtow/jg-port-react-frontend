97923241760bbc9e59cd1cc66244f71e96da2ddb
jessegumtow <gumtow@gmail.com>
2021-04-29 21:57:27 -0500

    updated header image and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SectionMargin } from "../styles/layout";
-import { Row, Container, Col } from "react-bootstrap";
+import { Row, Container, Col, Button } from "react-bootstrap";
 // import { FlexCenter } from "../styles/layout";
 
 const About = (props) => {
@@ -10,19 +10,27 @@ const About = (props) => {
         <SectionMargin>
           <Container>
             <Row>
-              <Col>
-                <img id="profile-img" src="https://api.jessegumtow.com/wp-content/uploads/2021/04/Circle_jg-profile.png" alt="Jesse Gumtow" />
-              </Col>
-              <Col className="align-items-center">
+              <Col md={6} className="align-items-center">
                 <div className="flex-div">
-                  <h1>Hello,</h1>
-                  <p>
+                  <h1 id="hello">Hello,</h1>
+                  <h5>
                     I’m Jesse, a human-centered, front end software developer creating
                     elegant solutions to complex problems. My creative personality
                     pushes me to find opportunities to learn and develop tools that
                     improve the lives of others.
-                </p>
+                </h5>
                 </div>
+              </Col>
+              <Col>
+                <img id="profile-img" src="https://api.jessegumtow.com/wp-content/uploads/2021/04/Circle_jg-profile.png" alt="Jesse Gumtow" />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={3}>
+                <Button variant="outline-light" block>work</Button>
+              </Col>
+              <Col md={3}>
+                <Button variant="outline-info" block>Let's Connect</Button>
               </Col>
             </Row>
           </Container>
